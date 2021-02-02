9c8ebbe66922112b26148a2e4d336ec34fa64862
keishaputritheanny <pkeisha47@gmail.com>
2021-02-02 14:48:20 +0700

    Initial commit

---
+++ app/src/main/java/com/kei/finalrecipeapp/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.kei.finalrecipeapp
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.auth.FirebaseAuth
+
+class MainActivity : AppCompatActivity() {
+
+    companion object{
+        fun getLaunchService(from: Context) = Intent(from, MainActivity::class.java).apply {
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        }
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setConfigurationUi()
+    }
+    private fun setConfigurationUi() {
+        supportActionBar?.title = "Recipe App"
+    }
+
+}
