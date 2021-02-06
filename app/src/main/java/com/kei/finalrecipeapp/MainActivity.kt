73bc12ab8a935d3aa400380d1397a779ac85d1b3
keishaputritheanny <pkeisha47@gmail.com>
2021-02-06 10:26:11 +0700

    button navigation  and fragments

--- app/src/main/java/com/kei/finalrecipeapp/MainActivity.kt
+++ app/src/main/java/com/kei/finalrecipeapp/MainActivity.kt
@@ -4,10 +4,14 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.google.firebase.auth.FirebaseAuth
+import android.view.Menu
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController : NavController
 
     companion object{
         fun getLaunchService(from: Context) = Intent(from, MainActivity::class.java).apply {
@@ -18,6 +22,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setConfigurationUi()
+        navController = findNavController(R.id.nav_host_fragment)
+        setupActionBarWithNavController(navController)
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        nav_bottom_view.setupWithNavController(menu!!, navController)
+        return true
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        navController.navigateUp()
+        return true
     }
     private fun setConfigurationUi() {
         supportActionBar?.title = "Recipe App"
